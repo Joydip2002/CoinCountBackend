fae69ed403061ef615a19f31bc6432c42850d97d
JOYDIP MANNA <JOYDIPMANNA2002@GMAIL.COM>
2024-09-15 00:33:57 +0530

    modify-15-09-2024

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,4 @@
+const { Sequelize, Op, QueryTypes } = require('sequelize');
 const models = require('../models');
 
 async function coincountSignup(req, res){
@@ -13,33 +14,34 @@ async function coincountSignup(req, res){
         };
         console.log("User Name: ", req_data.name);
         if (!req_data.email || !req_data.password || req_data.password !== req_data.cpassword) {
-            return res.status(400).json({
-                status: 400,
-                msg: "Invalid input: Please make sure passwords match and all fields are provided.",
+            res.status(200).json({
+                status: 401,
+                msg: "password mismatch",
                 data: { req_data }
             });
-        }
-        // Check if the user already exists
-        const checkUser = await models.users.findOne({
-            where: { email: req_data.email }
-        });
-        if (checkUser) {
-            return res.status(200).json({
-                status: 200,
-                msg: "User already exists",
-                data: req_data
+        }else{
+            // Check if the user already exists
+            const checkUser = await models.users.findOne({
+                where: { email: req_data.email }
             });
-        } else { 
-            const createUser = await models.users.create(req_data);       
-            return res.status(201).json({
-                status: 201,
-                msg: "Registration successful",
-                data: req_data
-            });
+            if (checkUser) {
+                res.status(200).json({
+                    status: 401,
+                    msg: "User already exists",
+                    data: req_data
+                });
+            } else { 
+                const createUser = await models.users.create(req_data);       
+                res.status(200).json({
+                    status: 200,
+                    msg: "Registration successful",
+                    data: req_data
+                });
+            }
         }
     }catch (error) {
         console.error("Error occurred - coincountSignup: ", error);
-        return res.status(500).json({
+        res.status(500).json({
             status: 500,
             msg: "Error occurred - coincountSignup: Post API"
         });
@@ -61,22 +63,23 @@ async function coinCountLogin(req,res){
                 msg: "Invalid input fields are provided.",
                 data: { req_data }
             })
-        }
-        const user=await models.users.findOne({
-            where:{email : req_data.email, password : req_data.password}
-        })
-        if(user){
-            res.status(200).json({
-                status: 200,
-                msg: "Login successfull",
-                data: { req_data }
-            })
         }else{
-            res.status(400).json({
-                status: 400,
-                msg: "Invalid credentials!",
-                data: { req_data }
+            const user=await models.users.findOne({
+                where:{email : req_data.email, password : req_data.password}
             })
+            if(user){
+                res.status(200).json({
+                    status: 200,
+                    msg: "Login successfull",
+                    data: req_data
+                })
+            }else{
+                res.status(200).json({
+                    status: 400,
+                    msg: "Invalid credentials!",
+                    data: req_data
+                })
+            }
         }
     }catch (error) {
         console.error("Error occurred - coinCountLogin: ", error);
@@ -152,7 +155,7 @@ async function updateUsersMoney(req,res){
                 const userMoney = await models.user_money_info.findOne({
                     where:{user_id : req_data.user_id}
                 });
-                console.log(userMoney);
+                // console.log(userMoney);
                 if(!userMoney){
                     await models.user_money_info.create(req_data);
                     res.status(200).json({
@@ -184,7 +187,44 @@ async function updateUsersMoney(req,res){
             });
         }
     }catch (error) {
-        
+        console.error("Error occurred - updateUsersMoney: ", error);
+        res.status(500).json({
+            status: 500,
+            msg: "Error occurred - updateUsersMoney: Post API"
+        });
+    }
+}
+
+async function fetchUserIdByEmail(req,res){
+    try{
+        const user_email = req.body.email || "";
+        if(user_email!=""){
+            const findUser=await models.users.findAll({
+                where:{email:user_email}
+            });
+            if(findUser!=""){
+                res.status(200).json({
+                    'status':200,
+                    'data':findUser
+                })
+            }else{
+                res.status(200).json({
+                    'status':200,
+                    'msg':"Email not exists"
+                })
+            }
+        }else{
+            res.status(200).json({
+                'status':200,
+                'msg':"email is required"
+            })
+        }
+    }catch (error) {
+        console.error("Error occurred - fetchUserIdByEmail: ", error);
+        res.status(500).json({
+            status: 500,
+            msg: "Error occurred - fetchUserIdByEmail: Post API"
+        });
     }
 }
 
@@ -192,12 +232,13 @@ async function fetchTransactions(req,res){
     try {
         if(req.params.user_id>0){
             const allTransactionsData=await models.user_transactions.findAll({
-                where:{user_id:req.params.user_id}
+                where:{user_id:req.params.user_id},
+                order: [['createdAt', 'DESC']]
             });
             res.status(200).json({
                 'status':200,
                 'msg':'fetching successful',
-                'data':[allTransactionsData]
+                'data':allTransactionsData
             });
         }else{
             res.status(400).json({
@@ -206,7 +247,138 @@ async function fetchTransactions(req,res){
             });
         }
     }catch (error) {
-        
+        console.error("Error occurred - fetchTransactions: ", error);
+        res.status(500).json({
+            status: 500,
+            msg: "Error occurred - fetchTransactions: Get API"
+        });
+    }
+}
+
+async function fetchIncomeExpenseDetails(req,res) {
+    try{
+        var response={};
+        if(req.params.userId>0){
+            const userCheck = await models.users.findOne({
+                where:{id:req.params.userId}
+            })
+            if(userCheck){
+                const startOfMonth = new Date();
+                startOfMonth.setDate(1);
+                startOfMonth.setHours(0, 0, 0, 0);
+
+                const endOfMonth = new Date(startOfMonth);
+                endOfMonth.setMonth(startOfMonth.getMonth() + 1);
+
+                // SELECT SUM(amount) AS Expense FROM `user_transactions` WHERE transaction_type='expense' AND user_id=3
+                const totalExpense = await models.user_transactions.sum('amount', {
+                    where: {
+                        user_id: req.params.userId,
+                        transaction_type: 'expense',
+                        createdAt: {
+                            [Op.between]: [startOfMonth, endOfMonth]
+                        }
+                    }
+                });
+                // income
+                const totalIncome = await models.user_transactions.sum('amount', {
+                    where: {
+                        user_id: req.params.userId,
+                        transaction_type: 'income',
+                        createdAt: {
+                            [Op.between]: [startOfMonth, endOfMonth]
+                        }
+                    }
+                });
+                response={
+                    'status':200,
+                    'msg':'fetched successfully',
+                    'details':{
+                        'expense':totalExpense??0,
+                        'income':totalIncome??0
+                    }
+                }
+            }else{
+                response={
+                    'status':400,
+                    'msg':'user not exists'
+                }
+            }
+        }else{
+            response={
+                'status':400,
+                'msg':"Invalid UserId"
+            }
+        }
+        res.json({
+            data:response
+        })
+    }catch (error) {
+        console.error("Error occurred - fetchIncomeExpenseDetails: ", error);
+        res.status(500).json({
+            status: 500,
+            msg: "Error occurred - fetchIncomeExpenseDetails: Get API"
+        });
+    }
+}
+
+async function todayTransactionDetails(req,res){
+    try {
+        var response={};
+        if(req.params.userId>0){
+            var userId= req.params.userId;
+            const userCheck = await models.users.findOne({
+                where:{id:req.params.userId}
+            })
+            if(userCheck){
+                var today = new Date();
+                var formattedDate = today.toISOString().split('T')[0];
+                var timestamp = formattedDate;
+                var datePart = timestamp.split(' ')[0];
+                const todayTransactionsData = await models.sequelize.query(`
+                    SELECT 
+                        id, 
+                        transaction_type, 
+                        amount, 
+                        user_id, 
+                        category, 
+                        description, 
+                        createdAt, 
+                        updatedAt 
+                    FROM user_transactions 
+                    WHERE DATE(updatedAt) = :datePart
+                      AND user_id = :userId
+                    ORDER BY id DESC 
+                `, {
+                    replacements: { userId, datePart },
+                    type: QueryTypes.SELECT
+                });
+                response={
+                    'status':200,
+                    'msg':'fetched successfully',
+                    'details': todayTransactionsData
+                }
+            }else{
+                response={
+                    'status':400,
+                    'msg':'user not exists'
+                }
+            }
+        }else{
+            response={
+                'status':400,
+                'msg':"Invalid UserId"
+            }
+        }
+        res.json({
+            data:response
+        })
+    } catch (error) {
+        console.error("Error occurred - todayTransactionDetails: ", error);
+        res.status(500).json({
+            status: 500,
+            msg: "Error occurred - todayTransactionDetails: Get API"
+        });
     }
 }
 
@@ -216,5 +388,8 @@ module.exports = {
     coinCountLogin:coinCountLogin,
     addTransactions:addTransactions,
     updateUsersMoney:updateUsersMoney,
-    fetchTransactions:fetchTransactions
+    fetchTransactions:fetchTransactions,
+    fetchUserIdByEmail:fetchUserIdByEmail,
+    fetchIncomeExpenseDetails:fetchIncomeExpenseDetails,
+    todayTransactionDetails:todayTransactionDetails
 };
--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,10 @@ router.post("/login",usersControler.coinCountLogin);
 
 router.post("/addTansactions",usersControler.addTransactions);
 router.post("/updateUsersMoney",usersControler.updateUsersMoney);
+router.post("/fetchUserIdByEmail",usersControler.fetchUserIdByEmail);
 router.get("/fetchTransactions/:user_id",usersControler.fetchTransactions);
+router.get("/fetchIncomeExpenseDetails/:userId",usersControler.fetchIncomeExpenseDetails);
+router.get("/todayTransactionDetails/:userId",usersControler.todayTransactionDetails);
 
 
 module.exports = router
